d7a69e56fe160b0575200224433cea2e75b87212
Cael Rowley <drdeus@pm.me>
2023-09-20 19:00:12 +0200

    chore: refactor project to use best practices

--- src/player.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-var speed := 100.0
-
-func _physics_process(delta):
-	var dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = dir * speed
-	move_and_slide()
